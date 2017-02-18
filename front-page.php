96406e7a65b6dbb7c9cd638f83a757276714a746
carranj <carranco.jose.r@gmail.com>
2017-02-18 17:19:42 -0600

    started singles-product template and called in the relationship to the homepage

--- front-page.php
+++ front-page.php
@@ -1,9 +1,50 @@
 <?php get_header(); ?> 
   <div class="container">
-      <?php if (have_posts()) : ?>
-      <?php while (have_posts()) : the_post(); ?>
-       
-      <?php endwhile; endif;?>
+    <div class="col-md-6">
+	    <?php
+	    $posts = get_field('featured_product_1');
+	    if($posts):
+			foreach ($posts as $post):
+			setup_postdata($post); 
+	    	?>
+	  		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	  		<?php if( have_rows('add_images') ): ?>
+	                <?php while ( have_rows('add_images') ) : the_row(); ?>
+	                      
+	            <!-- the_sub_field-->
+	                   
+	              <?php $productImage = get_sub_field('image'); ?>
+	              <img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" />
+	    
+	              <?php endwhile; else : ?>
+	            <?php endif; ?>
+	  	<?php endforeach; ?>
+	  	<?php wp_reset_postdata(); ?>
+	  	<?php endif; ?>
+	</div>
+  	 <div class="col-md-6">
+	    <?php
+	    $posts = get_field('featured_product_1');
+	    if($posts):
+			foreach ($posts as $post):
+			setup_postdata($post); 
+	    	?>
+	  		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	  		<?php if( have_rows('add_images') ): ?>
+	                <?php while ( have_rows('add_images') ) : the_row(); ?>
+	                      
+	            <!-- the_sub_field-->
+	                   
+	              <?php $productImage = get_sub_field('image'); ?>
+	              <img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" />
+	    
+	              <?php endwhile; else : ?>
+	            <?php endif; ?>
+	  	<?php endforeach; ?>
+	  	<?php wp_reset_postdata(); ?>
+	  	<?php endif; ?>
+	</div>
+      
   </div>
 <?php get_footer(); ?> 
 
---
+++ single-products.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+<div class="row page-header">
+          <div class="col-md-12 productIntroBanner">
+          <div class="col-md-2 logo">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/hsf-logo.svg">
+          </div>
+          <div class="col-md-9 offset-md-1">
+            <h1><?php the_title(); ?></h1>
+          </div>
+          
+          </div>
+      </div>
+
+    <div class="container">
+      <div class="row">       
+        <div class="col-md-12">
+          
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="col-md-6 productImage">
+             <?php if( have_rows('add_images') ): ?>
+                <?php while ( have_rows('add_images') ) : the_row(); ?>
+                      
+            <!-- the_sub_field-->
+                   
+              <?php $productImage = get_sub_field('image'); ?>
+              <img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" />
+    
+              <?php endwhile; else : ?>
+            <?php endif; ?>
+          </div>
+          <div class="col-md-6 productInformation">
+
+            <div class="productDescription">
+              <h2>Product Description</h2>
+              <?php the_field('description');?>
+            </div>
+
+            <div class="productDetails">
+              <h2>Product Details</h2>
+              <?php the_field('details');?>
+            </div>
+
+          </div>
+          <?php endwhile; else: ?>
+          
+          <div class="page-header">
+            <h1>Oh No!</h1>
+          </div>
+
+          <p>No Content is appearing for this page!</p>
+
+          <?php endif; ?>
+
+
+        </div>
+      </div>
+    </div>
+
+   <?php get_footer(); ?> 
