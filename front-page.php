1d22d1fef950b4dbb5d49b2ab98f72c034b0ab6b
carranj <carranco.jose.r@gmail.com>
2017-02-20 20:11:52 -0600

    adding taxonomy templates

--- front-page.php
+++ front-page.php
@@ -1,50 +1,77 @@
 <?php get_header(); ?> 
-  <div class="container">
+	<div class="container">
+		<div class="col-md-6">
+		    <?php
+		    $posts = get_field('featured_product_1');
+		    if($posts):
+				foreach ($posts as $post):
+				setup_postdata($post); 
+		    ?>
+		    
+		    <div class="grid">
+		      <figure class="effect-zoe">
+		        <a href="<?php the_permalink(); ?>">
+
+		        <?php if( have_rows('add_images') ): ?>
+		        <?php while ( have_rows('add_images') ) : the_row(); ?>
+		              
+		        <!-- the_sub_field-->
+		               
+		          <?php $productImage = get_sub_field('image'); ?>
+		          <a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" /></a>
+
+		          <?php endwhile; else : ?>
+		        <?php endif; ?>
+		        	
+		        </a>
+		        <figcaption>
+		        <div class="bottomtitle">
+		          <h2><?php the_title(); ?></h2>
+		          </div>
+		        </figcaption>
+		        <div class="clearfix"></div>    
+		      </figure>
+		      <?php endforeach; ?>
+		  	<?php wp_reset_postdata(); ?>
+		  	<?php endif; ?>
+		</div>
+	  		
+	</div>
     <div class="col-md-6">
 	    <?php
 	    $posts = get_field('featured_product_1');
 	    if($posts):
 			foreach ($posts as $post):
 			setup_postdata($post); 
-	    	?>
-	  		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	  		<?php if( have_rows('add_images') ): ?>
-	                <?php while ( have_rows('add_images') ) : the_row(); ?>
-	                      
-	            <!-- the_sub_field-->
-	                   
-	              <?php $productImage = get_sub_field('image'); ?>
-	              <img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" />
+	    ?>
 	    
-	              <?php endwhile; else : ?>
-	            <?php endif; ?>
-	  	<?php endforeach; ?>
+	    <div class="grid">
+	      <figure class="effect-zoe">
+	        <a href="<?php the_permalink(); ?>">
+
+	        <?php if( have_rows('add_images') ): ?>
+	        <?php while ( have_rows('add_images') ) : the_row(); ?>
+	              
+	        <!-- the_sub_field-->
+	               
+	          <?php $productImage = get_sub_field('image'); ?>
+	          <a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" /></a>
+
+	          <?php endwhile; else : ?>
+	        <?php endif; ?>
+	        	
+	        </a>
+	        <figcaption>
+	        <div class="bottomtitle">
+	          <h2><?php the_title(); ?></h2>
+	          </div>
+	        </figcaption>
+	        <div class="clearfix"></div>    
+	      </figure>
+	      <?php endforeach; ?>
 	  	<?php wp_reset_postdata(); ?>
 	  	<?php endif; ?>
-	</div>
-  	 <div class="col-md-6">
-	    <?php
-	    $posts = get_field('featured_product_1');
-	    if($posts):
-			foreach ($posts as $post):
-			setup_postdata($post); 
-	    	?>
-	  		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	  		<?php if( have_rows('add_images') ): ?>
-	                <?php while ( have_rows('add_images') ) : the_row(); ?>
-	                      
-	            <!-- the_sub_field-->
-	                   
-	              <?php $productImage = get_sub_field('image'); ?>
-	              <img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" />
-	    
-	              <?php endwhile; else : ?>
-	            <?php endif; ?>
-	  	<?php endforeach; ?>
-	  	<?php wp_reset_postdata(); ?>
-	  	<?php endif; ?>
-	</div>
-      
+	</div>  
   </div>
 <?php get_footer(); ?> 
 
---
+++ taxonomy-teams.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+  
+    <div class="container">
+    <div class="row page-header">
+          <div class="col-sm-12">
+            <h1><?php wp_title(); ?></h1>
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+
+
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <article class="post">
+            
+            <h2><a href ="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p><em> 
+              By <?php the_author(); ?> 
+              on <?php echo the_time ('l, F jS, Y');?>
+              in <?php the_category(', '); ?>
+              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em>
+            </p>
+            
+            <?php the_excerpt(); ?>
+            
+            <hr>
+
+          </article>
+          
+        <?php endwhile; else: ?>
+          
+          <div class="page-header">
+            <h1>Oh No!</h1>
+          </div>
+
+          <p>No Content is appearing for this page!</p>
+
+          <?php endif; ?>
+
+
+        </div>
+        
+      </div>
+      </div>
+
+   <?php get_footer(); ?> 
