2c51ae25639e0296b8360a25c7192adb1f9145bd
carranj <carranco.jose.r@gmail.com>
2017-02-23 01:51:42 -0600

    querying all posts for custom taxonomy on archive pages

--- archive.php
+++ archive.php
@@ -6,7 +6,7 @@
           </div>
       </div>
       <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-12">
 
 
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -39,8 +39,6 @@
 
 
         </div>
-        
-       <?php get_sidebar('blog'); ?>
         
       </div>
       </div>
--- taxonomy-teams.php
+++ taxonomy-teams.php
@@ -3,69 +3,60 @@
     <div class="row page-header">
       <div class="container">
      
-      <div class="col-md-2">
+        <div class="col-md-2">
 
-         <?php
- $term = get_queried_object()->slug;       
- 
-          if( $term === 'astros') : ?>
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston-Astros-Logo.svg">
-          
-          <?php elseif( $term === 'cougars') : ?>
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/University_of_Houston_Logo.svg">
-          
-          <?php elseif( $term === 'rockets') : ?>
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Rockets.svg">
-          
-          <?php elseif( $term === 'dynamo') : ?>
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Dynamo_logo.svg">
+         <?php 
 
-          <?php elseif( $term === 'texans') : ?>
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Texans_logo.svg">
+         $term = get_queried_object()->slug;       
           
-          <?php endif; ?>
+            if( $term === 'astros') : ?>
+              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston-Astros-Logo.svg">
+            
+            <?php elseif( $term === 'cougars') : ?>
+              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/University_of_Houston_Logo.svg">
+            
+            <?php elseif( $term === 'rockets') : ?>
+              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Rockets.svg">
+            
+            <?php elseif( $term === 'dynamo') : ?>
+              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Dynamo_logo.svg">
 
-      </div>
-          <div class="col-md-10">
-            <h1><?php wp_title(); ?></h1>
-          </div>
+            <?php elseif( $term === 'texans') : ?>
+              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/Houston_Texans_logo.svg">
+            
+            <?php endif; ?>
+
         </div>
+        <div class="col-md-10">
+          <h1><?php wp_title(); ?></h1>
+        </div>
+    </div>
       </div>
       <div class="container">
-        <div class="col-md-12">
-
-
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-          <article class="post">
-            
-            <h2><a href ="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <p><em> 
-              By <?php the_author(); ?> 
-              on <?php echo the_time ('l, F jS, Y');?>
-              in <?php the_category(', '); ?>
-              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em>
-            </p>
-            
-            <?php the_excerpt(); ?>
-            
-            <hr>
-
-          </article>
-          
-        <?php endwhile; else: ?>
-          
-          <div class="page-header">
-            <h1>Oh No!</h1>
+      <?php 
+       
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="col-md-3">
+          <div class="product">
+           <?php if( have_rows('add_images') ): ?>
+            <?php while ( have_rows('add_images') ) : the_row(); ?>
+                  
+            <!-- the_sub_field-->
+                  
+              <?php $productImage = get_sub_field('image'); ?>
+              <a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $productImage['url']; ?>" alt="<?php echo $productImage['alt']; ?>" /></a>
+             
+              <?php endwhile; else : ?>
+            <?php endif; ?>
           </div>
-
-          <p>No Content is appearing for this page!</p>
-
-          <?php endif; ?>
+          </div>
+         <?php endwhile; else : ?>
 
 
+          <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+
+         <?php endif; ?>
         </div>
-        
-      </div>
 
    <?php get_footer(); ?> 
